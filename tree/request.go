189a417129d2787ad65a96b913d1fd397d6c21e6
Taylor Lawson <taylorl@benon.com>
2018-07-01 17:17:28 +1000

    New directory request generators for better git detection

--- tree/request.go
+++ tree/request.go
@@ -2,6 +2,7 @@ package tree
 
 import (
 	"net/url"
+	"regexp"
 )
 
 type DirectoryRequest struct {
@@ -9,6 +10,35 @@ type DirectoryRequest struct {
 }
 
 func NewDirectoryRequest(userInput string) (new DirectoryRequest, err error) {
+	for _, generator := range drGenerators {
+		if ok, dr := generator(userInput); ok {
+			return dr, nil
+		}
+	}
 	new.url, err = url.Parse(userInput)
-	return
+	return new, err
+}
+
+func mapSubexpNames(m, n []string) map[string]string {
+	m, n = m[1:], n[1:]
+	r := make(map[string]string, len(m))
+	for i, _ := range n {
+		r[n[i]] = m[i]
+	}
+	return r
+}
+
+func checkForMatch(input string, regexes []*regexp.Regexp) (bool, map[string]string) {
+	for _, r := range regexes {
+		if m := r.FindStringSubmatch(input); len(m) > 0 {
+			return true, mapSubexpNames(m, r.SubexpNames())
+		}
+	}
+	return false, nil
+}
+
+type directoryRequestGenerator func(string) (bool, DirectoryRequest)
+
+var drGenerators = []directoryRequestGenerator{
+	attemptGitDirectoryRequest,
 }
---
+++ tree/requestGit.go
@@ -0,0 +1,21 @@
+package tree
+
+import (
+	"net/url"
+	"regexp"
+)
+
+var gitRegexes = []*regexp.Regexp{
+	regexp.MustCompile(`^\w+@(?P<Domain>[a-zA-Z0-9\.@-_]+):(?P<Path>.+.git)$`),                    // https://regex101.com/r/kLMnfa/4
+	regexp.MustCompile(`^(?:https?:\/\/)?(?P<Domain>[a-zA-Z0-9\.@-_]+)\/(?P<Path>(\/?.)+\.git)$`), // https://regex101.com/r/v2nXgX/7
+}
+
+func attemptGitDirectoryRequest(input string) (match bool, new DirectoryRequest) {
+	match, data := checkForMatch(input, gitRegexes)
+	new.url = &url.URL{
+		Host:    data["Domain"],
+		Path:    data["Path"],
+		RawPath: input,
+	}
+	return match, new
+}
