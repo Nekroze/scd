8f8109cbd52da544d8f680ba6de04b2cc26de395
Taylor Lawson <taylorl@benon.com>
2018-07-15 12:38:12 +1000

    Add -dryrun flag

--- cli/entrypoint.go
+++ cli/entrypoint.go
@@ -1,22 +1,33 @@
 package cli
 
 import (
+	"flag"
 	"fmt"
 	"os"
 
 	"github.com/Nekroze/scd/tree"
 )
 
+var dryRun bool
+
+func parseArgs() {
+	flag.BoolVar(&dryRun, "dryrun", false, "If true no files or directories will be created")
+	flag.Parse()
+}
+
 func Entrypoint() {
-	if len(os.Args) != 2 {
+	parseArgs()
+	if len(flag.Args()) != 1 {
 		fmt.Println(os.Getenv("HOME"))
 		return
 	}
-	pathInput := os.Args[1]
+	pathInput := flag.Arg(0)
 	req := tree.NewDirectoryRequest(pathInput)
 	dir := tree.NewDirectory(req)
-	if e := dir.Ensure(); e != nil {
-		panic(e)
+	if !dryRun {
+		if e := dir.Ensure(); e != nil {
+			panic(e)
+		}
 	}
 	fmt.Println(dir.Path)
 }
